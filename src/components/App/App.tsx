4efeb1076cc141e751ad36abe47f648c6b6e833a
VitaliiSakhno <vitalik.tarasovich@gmail.com>
2026-03-05 11:18:48 +0100

    added states

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,14 +1,26 @@
-// import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import css from "../App/App.module.css";
+
+import CafeInfo from "../CafeInfo/CafeInfo";
+import type { Votes } from "../../types/votes";
+import type { VoteType } from "../../types/votes";
 
 function App() {
-  // const [count, setCount] = useState(0)
+  const [votes, setVotes] = useState<Votes>({ good: 0, neutral: 0, bad: 0 });
+
+  function handleVote(type: VoteType) {
+    setVotes((previous) => ({ ...previous, [type]: previous[type] + 1 }));
+  }
+
+  function resetVotes() {
+    return setVotes({ good: 0, neutral: 0, bad: 0 });
+  }
 
   return (
-    <>
-      
-    </>
-  )
+    <div className={css.app}>
+      <CafeInfo />
+    </div>
+  );
 }
 
-export default App
+export default App;
